bbc384a25815491bf8c55ae20da14db94a5132b6
shae128 <slr772s@gmail.com>
2019-01-15 09:25:17 +0100

    The code can determine lose, and also draw the hangman so far.

--- hangman.py
+++ hangman.py
@@ -1,6 +1,7 @@
 from time import sleep
 from os import system
 
+
 def boardInit():
 
     hangmanBoard = [[" " for i in range(22)] for j in range(14)]
@@ -28,11 +29,13 @@ def boardInit():
     for i in range(11, 14):
         hangmanBoard[i][21] = "|"
 
-
     return hangmanBoard
 
 
-def drawBoard(hangmanBoard, errorNum):
+def drawBoard(hangmanBoard, errorNum, errorLetter=None):
+
+    if errorLetter != None:
+        hangmanBoard[0][errorNum-1] = errorLetter
 
     # if errorNum 1
     if errorNum >= 1:
@@ -52,11 +55,18 @@ def drawBoard(hangmanBoard, errorNum):
         for i in range(4, 5):
             hangmanBoard[i][19] = "'|"
 
+        # underline lips
+        # hangmanBoard[4][17] = u"\u23DC"
+        hangmanBoard[4][17] = "_"
+        # hangmanBoard[4][17] = u"\u23D1"
+
         # if errorNum 2
     if errorNum >= 2:
         # hands
         hangmanBoard[7][17] = "/"
         hangmanBoard[7][19] = "\\"
+        # hangmanBoard[7][17] = "\\"
+        # hangmanBoard[7][19] = "/"
 
         # if errorNum 3
     if errorNum >= 3:
@@ -96,10 +106,9 @@ def drawBoard(hangmanBoard, errorNum):
     except:
         system('cls')  # for windows
 
+    print("You are " + str(6 - errorNum) + " moves away from hanging the man!\n")
+    print("Mistakes:")
 
-
-    # clear screen
-    # print(chr(27) + "[2J")
 
     for i in hangmanBoard:
 
@@ -108,8 +117,58 @@ def drawBoard(hangmanBoard, errorNum):
 
         print("")
 
+    print("")
+    print("")
+
 
 gameBoard = boardInit()
-for i in range(7):
-    drawBoard(gameBoard, i)
-    sleep(1)
+
+# for i in range(7):
+#     drawBoard(gameBoard, i)
+#     sleep(1)
+
+
+def gameRun():
+
+    wordToGuess = list(input("Select your word: ").upper())
+    toPrintOut = []
+    errorNum = 0
+
+    for i in range(len(wordToGuess)):
+        if wordToGuess[i] is " ":
+            toPrintOut.append(" ")
+        else:
+            toPrintOut.append("_")
+
+        drawBoard(gameBoard, errorNum)
+
+        for i in toPrintOut:
+            print(i, end=" ")
+
+        print("\n\n\n")
+
+    while errorNum < 6:
+        found = False
+        decision = input("Enter your guess: ").upper()
+
+        if len(decision) != 1:
+            print("Please enter just one letter.")
+            continue
+
+        for i in range(len(wordToGuess)):
+            if wordToGuess[i] == decision:
+                toPrintOut[i] = decision
+                found = True
+
+        if not found:
+            errorNum += 1
+
+        drawBoard(gameBoard, errorNum, decision + " ")
+
+        for i in toPrintOut:
+            print(i, end=" ")
+
+        print("\n\n\n")
+
+
+gameRun()
